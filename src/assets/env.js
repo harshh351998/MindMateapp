646f744163c454627fa6ae8b216fd4cf7d5a90d6
harshh351998 <mendapara.harsh47@gmail.com>
2025-04-09 10:42:43 +0530

    #02:- fix issues

---
+++ src/assets/env.js
@@ -0,0 +1,8 @@
+(function(window) {
+  window._env_ = window._env_ || {};
+  
+  // API URL configuration
+  window._env_.apiUrl = window._env_.apiUrl || "http://localhost:4200";
+  
+  // Any other environment variables needed for the app
+})(this); 
